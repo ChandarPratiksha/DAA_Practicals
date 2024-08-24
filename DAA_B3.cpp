d1cd5e8769afc02e692e7aee8f5c5393208d2617
Pratiksha Balasaheb Chandar <101510109+ChandarPratiksha@users.noreply.github.com>
2024-08-24 17:59:00 +0530

    DAA_B3.cpp

--- DAA_B3.cpp
+++ DAA_B3.cpp
@@ -6,7 +6,7 @@ using namespace std;
 // all the task can be
 // completed by 'per_day'
 // number of task per day
-bool valid(int per_day,
+bool check(int per_day,
 		vector<int> task, int d)
 {
 
@@ -23,21 +23,21 @@ bool valid(int per_day,
 		cur_day += day_req;
 
 		// If more days required
-		// than 'd' days so invalid
+		// than 'd' days so incheck
 
 		if (cur_day > d) {
 			return false;
 		}
 	}
 
-	// Valid if days are less
+	// check if days are less
 	// than or equal to 'd'
 	return cur_day <= d;
 }
 
 // Function to find minimum
 // task done each day
-int minimumTask(vector<int> task, int d)
+int min_task(vector<int> task, int d)
 {
 
 	int low = 0;
@@ -58,9 +58,9 @@ int minimumTask(vector<int> task, int d)
 				+ (high - low) / 2;
 
 		// If 'mid' number of task per day
-		// is valid so store as answer and
+		// is check so store as answer and
 		// more to first half
-		if (valid(mid, task, d)) {
+		if (check(mid, task, d)) {
 			per_day_task = mid;
 			high = mid - 1;
 		}
@@ -80,7 +80,7 @@ int main()
 	vector<int> task{ 3, 4, 7, 15 };
 	int D = 10;
 
-	cout << minimumTask(task, D) << endl;
+	cout << min_task(task, D) << endl;
 
 	return 0;
 }
